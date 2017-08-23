d6e46577ecc4ae1a859104fdaf6c0b6cc081be32
Emilio García Granda <emiliogg84@live.com>
2017-08-23 14:37:47 -0400

    Add context menu items for 'Exit, Change Credentials' and others

--- src/FileImporterApp/MyApplicationContext.cs
+++ src/FileImporterApp/MyApplicationContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace FileImporterApp
@@ -17,6 +18,34 @@ namespace FileImporterApp
         {
             contextMenu = new ContextMenu();
 
+            var separator1MenuItem = new MenuItem { Text = "-" };
+
+            var separator2MenuItem = new MenuItem { Text = "-" };
+
+            var separator3MenuItem = new MenuItem { Text = "-" };
+
+            var openImportFolderMenuItem = new MenuItem { Text = Resources.OpenImportFolderText };
+            openImportFolderMenuItem.Click += OpenImportFolderMenuItem_Click;
+
+            var importFilesMenuItem = new MenuItem { Text = Resources.ImportFilesText };
+            importFilesMenuItem.Click += ImportFilesMenuItem_Click;
+
+            var configureImportFolderMenuItem = new MenuItem { Text = Resources.ConfigureImportFolderText };
+            configureImportFolderMenuItem.Click += ConfigureImportFolderMenuItem_Click;
+
+            var changeCredentialsMenuItem = new MenuItem { Text = Resources.ChangeCredentialsText };
+            changeCredentialsMenuItem.Click += ChangeCredentialsMenuItem_Click;
+
+            var settingsMenuItem = new MenuItem { Text = Resources.SettingsText };
+            settingsMenuItem.Click += SettingsMenuItem_Click;
+
+            var exitMenuItem = new MenuItem { Text = Resources.ExitText };
+            exitMenuItem.Click += ExitMenuItem_Click;
+
+            contextMenu.MenuItems.AddRange(new MenuItem[]{openImportFolderMenuItem,
+            importFilesMenuItem, separator1MenuItem, configureImportFolderMenuItem, settingsMenuItem,
+            separator2MenuItem, changeCredentialsMenuItem, separator3MenuItem, exitMenuItem});
+
             notifyIcon = new NotifyIcon
             {
                 ContextMenu = contextMenu,
@@ -24,6 +53,36 @@ namespace FileImporterApp
                 Icon = Icon.ExtractAssociatedIcon("FileImporterApp.ico"),
                 Visible = true
             };
+        }
+
+        private void SettingsMenuItem_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ChangeCredentialsMenuItem_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ConfigureImportFolderMenuItem_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ImportFilesMenuItem_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OpenImportFolderMenuItem_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
