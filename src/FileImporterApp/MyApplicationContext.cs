3fe7bb21aceafedeb3f05a9aa76c3f36ec4bc1d1
Emilio García Granda <emiliogg84@live.com>
2017-08-23 14:09:26 -0400

    Add notification icon to the Windows TaskBar

--- src/FileImporterApp/MyApplicationContext.cs
+++ src/FileImporterApp/MyApplicationContext.cs
@@ -1,8 +1,29 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
 
 namespace FileImporterApp
 {
     class MyApplicationContext : ApplicationContext
     {
+        private NotifyIcon notifyIcon;
+        private ContextMenu contextMenu;
+
+        public MyApplicationContext()
+        {
+            InitializeContext();
+        }
+
+        private void InitializeContext()
+        {
+            contextMenu = new ContextMenu();
+
+            notifyIcon = new NotifyIcon
+            {
+                ContextMenu = contextMenu,
+                Text = "FileImporter",
+                Icon = Icon.ExtractAssociatedIcon("FileImporterApp.ico"),
+                Visible = true
+            };
+        }
     }
 }
